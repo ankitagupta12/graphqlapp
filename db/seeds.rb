6b95ba8851fcb5e62b367084a7177a42d002cbee
Sean Nguyen <sean.nguyentrong@gmail.com>
2018-10-23 22:17:23 +0800

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tables = 3.times.map do |index|
+  Table.create!(name: "TABLE_#{index}")
+end
+
+customers = tables.map do |table|
+  3.times.map do
+    Customer.create!(
+        name: Faker::Name.name,
+        table: table
+    )
+  end
+end.flatten
+
+customers.each do |customer|
+  2.times.each do
+    Item.create!(
+            customer: customer,
+            name: Faker::Food.dish,
+            status: rand(3)
+    )
+  end
+end
